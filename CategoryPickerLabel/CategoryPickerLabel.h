abea9fee68a6c9466d36b588bd0eda57ff20d912
Brent Michalski <Brent.Michalski@gmail.com>
2019-01-16 09:04:58 -0600

    Initial Commit

---
+++ CategoryPickerLabel/CategoryPickerLabel.h
@@ -0,0 +1,19 @@
+//
+//  CategoryPickerLabel.h
+//  CategoryPickerLabel
+//
+//  Created by Brent Michalski on 1/16/19.
+//  Copyright © 2019 Perlguy, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CategoryPickerLabel.
+FOUNDATION_EXPORT double CategoryPickerLabelVersionNumber;
+
+//! Project version string for CategoryPickerLabel.
+FOUNDATION_EXPORT const unsigned char CategoryPickerLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CategoryPickerLabel/PublicHeader.h>
+
+
